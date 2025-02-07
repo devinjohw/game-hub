e8188024172ce09b6e9c64c841ac5439b14f387f
Devin Johw <devin.johw@gmail.com>
2025-02-07 12:58:37 +0800

    Test Colored Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Button, HStack } from "@chakra-ui/react"
+import { Button } from "@chakra-ui/react"
 
 function App() {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Button colorPalette='cyan'>Click me</Button>
   )
 }
 
